b756f38ec90b5589613d92982fb01f41b1f83969
Prakhar Pratap Singh <142224254+prakharpratapsingh21@users.noreply.github.com>
2025-07-28 17:13:04 +0530

    Update Projects.js

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -9,7 +9,7 @@ import Proj6 from "../images/proj6.jpg"
 import Image from "next/image";
 
 const projects = [
-  { id: 1, image: Proj1, alt: "Project 1" src:"https://pixarc.vercel.app/" },
+  { id: 1, image: Proj1, alt: "Project 1", src:"https://pixarc.vercel.app/" },
   { id: 2, image: Proj2, alt: "Project 2" },
   { id: 3, image: Proj3, alt: "Project 3" },
   { id: 4, image: Proj4, alt: "Project 4" },
